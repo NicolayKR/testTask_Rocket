1f6075cbe66e43550c8a72f6feb54a2f639ddb8f
Nicolay <krjuchkovkolja@mail.ru>
2021-06-01 21:44:04 +0300

    Last versions

--- resources/js/components/slider-item.vue
+++ resources/js/components/slider-item.vue
@@ -25,54 +25,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.item_wrapp{
-    max-width: 349px;
-    height: 243px;
-    border: 1px solid #D8D8D8;
-    box-sizing: border-box;
-    background-color: white;
-    margin-left: 32px;
-    z-index: 1;
-    
-    &:nth-child(1){
-        margin-left: 0px;
-        }
-}
-.item_date{
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 16px;
-    color: #636363;
-    margin-top: 34px;
-    margin-left: 27px;
-}
-.item_title{
-    font-weight: 900;
-    font-size: 18px;
-    line-height: 24px;
-    color: #2B2B2B;
-    margin-top: 11px;
-    margin-left: 27px;
-}
-.item_button{
-    background-color: white;
-    border: 1px solid #D51522;
-    box-sizing: border-box;
-    filter: drop-shadow(0px 2px 8px rgba(65, 65, 65, 0.25));
-    margin-top: 75px;
-    margin-left: 27px;
-    height: 44px;
-    width: 156px;
-    a{
-        color: #D51522;
+.item{
+    &_wrapp{
+        max-width: 349px;
+        min-height: 243px;
+        border: 1px solid #D8D8D8;
+        box-sizing: border-box;
+        background-color: white;
+        margin-left: 32px;
+        z-index: 1;
+        &:nth-child(1){
+            margin-left: 0px;
+            }
+    }
+    &_date{
+        font-weight: 400;
         font-size: 14px;
         line-height: 16px;
-        font-weight: 500;
-        &:hover{
-            color:red;
+        color: #636363;
+        margin-top: 34px;
+        margin-left: 27px;
+    }
+    &_title{
+        font-weight: 900;
+        font-size: 18px;
+        line-height: 24px;
+        color: #2B2B2B;
+        margin-top: 11px;
+        margin-left: 27px;
+    }
+    &_button{
+        background-color: white;
+        border: 1px solid #D51522;
+        box-sizing: border-box;
+        filter: drop-shadow(0px 2px 8px rgba(65, 65, 65, 0.25));
+        margin-top: 75px;
+        margin-left: 27px;
+        height: 44px;
+        width: 156px;
+        a{
+            color: #D51522;
+            font-size: 14px;
+            line-height: 16px;
+            font-weight: 500;
+            &:hover{
+                color:red;
+                }
+            }
         }
     }
-}
 @media ( max-width: 990px ){
     .item_wrapp{
         max-width: 316px;
@@ -82,7 +83,6 @@ export default {
 @media ( max-width: 375px ){
     .item_wrapp{
         max-width: 300px;
-        min-height: 243;
         margin: 0 auto;
     }
 }
